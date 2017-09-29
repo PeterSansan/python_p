1c33fef79980f2851d94e6196e2025251263f25e
PeterSansan <1657225860@qq.com>
2017-09-29 17:35:54 +0800

    add

--- src/how_numpy.py
+++ src/how_numpy.py
@@ -1,9 +1,10 @@
-# -*- coding: utf-8 -*-
+
 """
 Created on Thu Sep 14 13:08:32 2017
 
 @author: PeterSansan
 """
-
-import numpy as np
-a = np.array([2,3,4])
+import string
+str = raw_input()
+str_list = string.split(str,'->')
+print(str_list)
